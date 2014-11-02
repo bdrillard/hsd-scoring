cee4aae5dd2a38f9bc3a2b5755fa1c55c6b1f8e9
Aleksander Eskilson <aleksanderesk@gmail.com>
2014-11-02 17:19:19 -0600

    updated forms for multiple forms

--- src/cljs/forms.cljs
+++ src/cljs/forms.cljs
@@ -4,20 +4,28 @@
             [dommy.core :as d])
   (:require-macros [dommy.macros :refer [sel1 node]]))
 
-(def update-score
+(def create-team
+  {:fields [{:name :h2 :type :heading :text "Create a team"}
+            {:name :team_name}]})
+
+(def update-team
   {:fields [{:name :h2 :type :heading :text "Update team data"}
             {:name :team_name}
             {:name :weight :datatype :float}
-            {:name :competition :type :select :options ["Launch" "Ramp" "Presentation"]}
+            {:name :competition :type :select :options ["--" "Launch" "Ramp" "Presentation"]}
             {:name :score :datatype :int}]})
 
+(def forms
+  [create-team
+   update-team])
+
 (defn main []
-  (.log js/console "Hello, world!")
-  (when-let [container (sel1 "#update-team")]
-    (d/append! container (node (f/render-form update-score)))
-    (fd/handle-submit
-                      update-score container
-      (fn [params]
-        (js/alert (pr-str params))))))
+  (when-let [container (sel1 "#forms")]
+    (doseq [form forms]
+      (d/append! container (node (f/render-form form)))
+      (fd/handle-submit
+                        form container
+        (fn [params]
+          (js/alert (pr-str params)))))))
 
 (main)
