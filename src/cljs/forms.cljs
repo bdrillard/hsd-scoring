566b785ccff2c4fdb3e620a1a1684a180b5d97a0
Aleksander Eskilson <aleksanderesk@gmail.com>
2014-11-02 16:51:49 -0600

    adding forms

---
+++ src/cljs/forms.cljs
@@ -0,0 +1,22 @@
+(ns hsd-scoring.forms
+  (:require [formative.core :as f]
+            [formative.dom :as fd]
+            [dommy.core :as d])
+  (:require-macros [dommy.macros :refer [sel1 node]]))
+
+(def update-score
+  {:fields [{:name :h2 :type :heading :text "Update team data"}
+            {:name :team_name}
+            {:name :weight :datatype :float}
+            {:name :competition :type :select :options ["Launch" "Ramp" "Presentation"]}
+            {:name :score :datatype :int}]})
+
+(defn main []
+  (when-let [container (sel1 "#update-team")]
+    (d/append! container (node (f/render-form update-score)))
+    (fd/handle-submit
+                      update-score container
+      (fn [params]
+        (js/alert (pr-str params))))))
+
+(main)
